2d8c964d034623dd7519770c00bb420dad03687f
Fredrik Sunding <fredrik.sunding@gmail.com>
2019-12-23 16:47:00 +0100

    Initial style adjustments

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
+/* import './reset.css' */
+import './style.scss'
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
     </div>
-  );
+  )
 }
 
 export default App
