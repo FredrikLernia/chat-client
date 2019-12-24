62910982e179644bbcc963a444ed285fbaff38d8
Fredrik Sunding <fredrik.sunding@gmail.com>
2019-12-24 09:23:11 +0100

    Created LoggedIn view

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,12 @@
 import React from 'react'
 /* import './reset.css' */
 import './style.scss'
-
-import Menu from './components/Menu'
-import Tab from './components/Tab'
+import LoggedIn from './views/LoggedIn'
 
 const App = () => {
   return (
     <div className="App">
-      <Menu />
-      <Tab />
+      <LoggedIn />
     </div>
   )
 }
---
+++ client/src/views/LoggedIn/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Menu from '../../components/Menu'
+import Tab from '../../components/Tab'
+import Main from '../../components/Main'
+
+const LoggedIn = () => {
+  return (
+    <>
+      <Menu />
+      <Tab />
+      <Main />
+    </>
+  )
+}
+
+export default LoggedIn
