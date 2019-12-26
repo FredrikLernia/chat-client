909ac1fdc3caa959124239575c8e637b57f8f6af
Fredrik Sunding <fredrik.sunding@gmail.com>
2019-12-26 14:27:15 +0100

    Includes chat list in tab

--- client/src/components/Tab/index.js
+++ client/src/components/Tab/index.js
@@ -2,11 +2,13 @@ import React from 'react'
 import './style.scss'
 
 import TabHeader from '../TabHeader'
+import ChatList from '../ChatList'
 
 const Tab = () => {
   return (
     <div className="Tab">
       <TabHeader />
+      <ChatList />
     </div>
   )
 }
