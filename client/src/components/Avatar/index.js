24da419d2a93bc0cf8dd2fa9041f3aecb2008cc4
Fredrik Sunding <fredrik.sunding@gmail.com>
2019-12-26 14:22:33 +0100

    Adjustments on Avatar component

--- client/src/components/Avatar/index.js
+++ client/src/components/Avatar/index.js
@@ -1,19 +1,16 @@
 import React from 'react'
 import './style.scss'
 
-// Temporary user object
-const user = require('../../json/user.json')
-
 const Avatar = props => {
   const style = {
     size: props.size ? ' ' + props.size : '',
-    color: user.colorTheme ? ' ' + user.colorTheme : ''
+    color: props.color ? ' ' + props.color : ''
   }
 
   return (
-    <div className={`Avatar${style.size}${style.color}`}>
-      <span>{user.firstName[0] + user.lastName[0]}</span>
-    </div>
+    <div className={`Avatar${style.color}`}>
+      <span className={style.size}>{props.initials}</span>
+    </div> 
   )
 }
 
