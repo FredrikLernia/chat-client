e6f99aba241afecbea2ad5c2ebb3174cb85518b0
Fredrik Sunding <fredrik.sunding@gmail.com>
2019-12-27 12:07:00 +0100

    Created ChatHeader component

---
+++ client/src/components/ChatHeader/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.scss'
+
+const ChatHeader = () => {
+  return (
+    <div className="ChatHeader">
+      <h2><span />Kajsa Anka</h2>
+    </div>
+  )
+}
+
+export default ChatHeader
