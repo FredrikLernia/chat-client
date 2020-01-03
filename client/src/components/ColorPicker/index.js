1c9829e12d94cbdb2a184c8c3e739ab67cea449a
Fredrik Sunding <fredrik.sunding@gmail.com>
2020-01-03 14:29:44 +0100

    Box shadow on ColorPicker works like on inputs

--- client/src/components/ColorPicker/index.js
+++ client/src/components/ColorPicker/index.js
@@ -1,15 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import { ChevronDown } from 'react-feather'
 import './style.scss'
 
+import UserContext from '../../context/UserContext'
+
 const ColorPicker = ({ color, changeColor }) => {
+  const { user } = useContext(UserContext)
+
   const [pickerOpen, setPickerOpen] = useState(false)
   const colors = ['yellow', 'green', 'red', 'blue', 'orange', 'wine', 'purple']
+
+  const shadow = user ? ` shadow-${user.colorTheme}` : ''
 
   return (
     <div className="ColorPicker">
       <label htmlFor="colorTheme">Color Theme</label>
-      <div className={pickerOpen ? 'color-picker open' : 'color-picker'} onClick={() => setPickerOpen(!pickerOpen)}>
+      <div className={pickerOpen ? `color-picker open${shadow}` : `color-picker${shadow}`} onClick={() => setPickerOpen(!pickerOpen)}>
         <div className={`color bg-${color}`}></div>
         <ChevronDown />
       </div>
