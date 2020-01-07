1cf6344fde6e67e0da5e62493a38f331f712fc7d
Fredrik Sunding <fredrik.sunding@gmail.com>
2020-01-07 21:08:59 +0100

    Update context after new message and empty the textarea

--- client/src/components/NewMessage/index.js
+++ client/src/components/NewMessage/index.js
@@ -2,17 +2,23 @@ import React, { useContext, useRef } from 'react'
 import { Send } from 'react-feather'
 import './style.scss'
 
+import useSetUser from '../../functions/useSetUser'
+
 import UserContext from '../../context/UserContext'
 import PageContext from '../../context/PageContext'
 
 import Avatar from '../Avatar'
 
 const NewMessage = () => {
+  const { update } = useSetUser()
+
   const { user } = useContext(UserContext)
   const { pageFriendship } = useContext(PageContext)
   const text = useRef()
 
   const { firstName, lastName, colorTheme } = user
+
+  const { friend } = user.friendships.find(friendship => friendship._id === pageFriendship)
 
   const onSubmit = async e => {
     e.preventDefault()
@@ -24,14 +30,16 @@ const NewMessage = () => {
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ friendshipId: pageFriendship._id, text: text.current.value })
+        body: JSON.stringify({ friendshipId: pageFriendship, text: text.current.value })
       })
+      update()
+      text.current.value = ''
     }
   }
 
   return (
     <>
-      <div className="typing">{pageFriendship.friend.firstName} skriver<span>.</span><span>.</span><span>.</span></div>
+      <div className="typing">{friend.firstName} skriver<span>.</span><span>.</span><span>.</span></div>
       <div className="NewMessage">
         <Avatar size="lg" initials={firstName[0] + lastName[0]} color={colorTheme} />
         <form onSubmit={onSubmit}>
