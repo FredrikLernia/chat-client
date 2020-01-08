82e0646ab1a4dbf1580ac37ee9a09fbbd1e1d6ba
Fredrik Sunding <fredrik.sunding@gmail.com>
2020-01-08 16:00:39 +0100

    Changed name on update function for easier understanding in the code

--- client/src/functions/useSetUser.js
+++ client/src/functions/useSetUser.js
@@ -5,11 +5,11 @@ import UserContext from '../context/UserContext'
 export default () => {
   const { setUser } = useContext(UserContext)
 
-  const update = async () => {
+  const updateUser = async () => {
     const loggedInRaw = await fetch('/api/login')
     const loggedIn = await loggedInRaw.json()
     typeof loggedIn === 'object' && setUser(loggedIn)
   }
 
-  return { update }
+  return { updateUser }
 }
