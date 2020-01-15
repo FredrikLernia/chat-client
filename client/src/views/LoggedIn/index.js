f8f74db7c7c5742fa68068c28404576fc1d445a2
Fredrik Sunding <fredrik.sunding@gmail.com>
2020-01-15 11:10:34 +0100

    SSE listening for "message" and updates user context

--- client/src/views/LoggedIn/index.js
+++ client/src/views/LoggedIn/index.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react'
+import SSE from 'easy-server-sent-events/sse'
+
+import useSetUser from '../../functions/useSetUser'
 
 import PageContext from '../../context/PageContext'
 
@@ -7,11 +10,23 @@ import Menu from '../../components/Menu'
 import Tab from '../../components/Tab'
 import Main from '../../components/Main'
 
+const sse = new SSE('/api/sse')
+let sseListenerAdded = false
+
 const LoggedIn = () => {
+  const { updateUser } = useSetUser()
+
   const [tab, setTab] = useState('chats')
   const [friendView, setFriendView] = useState('list')
   const [pageFriendship, setPageFriendship] = useState(null)
   const [infoQueue, setInfoQueue] = useState([])
+
+  if (!sseListenerAdded) {
+    sse.listen('message', () => {
+      updateUser()
+    })
+    sseListenerAdded = true
+  }
 
   return (
     <PageContext.Provider value={{
